42f9ba3f3c1032bae534cae884340305c1d64d29
JanaDemaskevits <shantal86@inbox.ru>
2021-12-19 08:06:16 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,19 +1,13 @@
 library(shiny)
 library(ggcorrplot)
-library(shinyjs)
 
 
 
 
 # Load data ----
-wine <- read.csv("wineQualityReds.csv",header=T,sep=",")
-wine <- wine[,-1]  # ID väli eemaldamine
-
-
-
-corr <- cor(wine)
-
-variables <- names(wine)
+wine <- read.csv("data/wineQualityReds.csv",header=T,sep=",")
+corr <- cor(wine[-1])
+variables <- names(wine[-1])
 
 # Define UI for app ----
 ui <- fluidPage(
@@ -33,25 +27,26 @@ ui <- fluidPage(
   
    
   
-  mainPanel(
+  mainPanel(width = '1000px',
 
     # Output: Tabset w/ plot, summary, and table ----
-    tabsetPanel(id="tabs1", type = "tabs",
+    tabsetPanel(id="tabs1",  type = "tabs",
                 tabPanel("SISSEJUHATUS", 
                          sidebarLayout( 
                            sidebarPanel( 
-                             img(src="wine.jpg", height='280px',width='210px')
+                             img(src="wine2.jpg", height='280px',width='355px')
                              ),
                          mainPanel(h3("Sissejuhatus"),
                                    br(),
-                                   p("Selle projekti autor valis analüüsimiseks andmestiku Red Wine Dataset, mis sisaldab teavet punaste veinide keemiliste omaduste kohta, samuti professionaalide hinnanguid punaste veinide kvaliteedile. Andmestik on võetud https://www.kaggle.com/piyushgoyal443/red-wine-dataset veebilehe andmestikude andmebaasist. Andmestik on .csv formaadis. Valitud andmestik on uurimiseks avalik.[1]"),
-                                   br(),
+                                   p("Selle projekti autor valis analüüsimiseks andmestiku Red Wine Dataset, mis sisaldab teavet punaste veinide keemiliste omaduste kohta, samuti professionaalide hinnanguid punaste veinide kvaliteedile. Andmestik on võetud ",
+                                   a("https://www.kaggle.com/piyushgoyal443/red-wine-dataset", href = "https://www.kaggle.com/piyushgoyal443/red-wine-dataset"), "veebilehe andmestikude andmebaasist. Andmestik on .csv formaadis. Valitud andmestik on uurimiseks avalik.[P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis. Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553. ISSN: 0167-9236.[WWW] http://dx.doi.org/10.1016/j.dss.2009.05.016 [Pre-press (pdf)] http://www3.dsi.uminho.pt/pcortez/winequality09.pdf [bib] http://www3.dsi.uminho.pt/pcortez/dss09.bib (05.12.2021)]"),
+                                   
                                    h3("Projekti eesmärgid:"),
                                    br(),
-                                   p("1. Tutvustada andmetega."), 
-                                   p("2. Teostada andmete esialgse visuaalse analüüsi sõltuvuste defineerimiseks."), 
-                                   p("3. Kursuses 'Andmete visualiseerimine' omandatud meetodite ja teadmiste rakendamine.")
-                         ))),
+                                   p("Projekti eesmärk on viia läbi kirjeldav analüüs, uurida ja leida punase veini kirjeldavate andmete sõltuvusi ning nende mõju veini kvaliteedile.
+Ning ka vaadata kas teised muutujad on omavahel korrelatsioonis. Tulemuste visualiseerimiseks kasutada kursuses 'Andmete visualiseerimine' omandatud meetodid ja teadmised."), 
+                         br(),
+                                   ))),
                 tabPanel("ANDMED",
                          sidebarLayout(
                          sidebarPanel( h3("Tunnused:"),
@@ -65,7 +60,7 @@ ui <- fluidPage(
                                        p(strong("free sulfur dioxide"), "(mg / dm^3): SO2 vaba vorm eksisteerib tasakaalus molekulaarse SO2 (lahustunud gaasina) ja vesiniksulfiidi iooni vahel; see takistab mikroobide kasvu ja veini oksüdeerumist"),
                                        p(strong("total sulfur dioxide"),"(mg / dm^3): S02 vabade ja seotud vormide kogus; madalates kontsentratsioonides ei ole SO2 veinis enamasti tuvastatav, kuid üle 50 ppm vaba SO2 kontsentratsiooni korral ilmneb SO2 veini ninas ja maitses"),
                                        p(strong("density"),"(g / cm^3): vee tihedus on lähedane vee tihedusele, sõltuvalt alkoholi- ja suhkrusisalduse protsendist"),
-                                       p(strong("pH"), ": kirjeldab, kui happeline või aluseline on vein skaalal 0 (väga happeline) kuni 14 (väga aluseline); enamik veine jääb pH skaalal vahemikku 3-4"),
+                                       p(strong("pH"), "kirjeldab, kui happeline või aluseline on vein skaalal 0 (väga happeline) kuni 14 (väga aluseline); enamik veine jääb pH skaalal vahemikku 3-4"),
                                        p(strong("sulphates"),"(potassium sulphate - g / dm3): veinilisand, mis võib soodustada vääveldioksiidi (S02) taset, mis toimib antimikroobse ja antioksüdandina"),
                                        p(strong("alcohol")," (% by volume): veini alkoholisisalduse protsent"),
                                        p(strong("quality"),"(score between 0 and 10) - professionaalide hinnanguid punaste veinide kvaliteedile")
@@ -74,25 +69,60 @@ ui <- fluidPage(
                            br(),
                            h3("Andmed:"), dataTableOutput("tabel")
                          ))),
+
+                tabPanel("KIRJELDAV ANALÜÜS",
+                         sidebarLayout(
+                           sidebarPanel(
+                                        selectInput('selectVar', "Vali tunnust:", variables, selected = variables[[1
+                                        ]]),
+                           br(),
+                           sliderInput(inputId = "bins",
+                                       label = "Vali kaalude arv:",
+                                       min = 1,
+                                       max = 50,
+                                       value = 30
+                                       ),
+                           ),
+                           
+                           mainPanel(br(),
+                                     
+                                     fluidRow(
+                                       column(6,
+                                              h3("Histogramm:"),
+                                              plotOutput(outputId = "distPlot"),
+                                       ),
+                                       column(6,
+                                              h3("Karp-vurrud diagramm:"),
+                                              plotOutput("boxplot"),
+                                       )
+                                     ),
+                             
+                             
+                             h3("Arvkarakteristikud:"),
+                             br(),    
+                             dataTableOutput("table2"),
+                             dataTableOutput("table3")
+                             
+                           ) )),
                 tabPanel("KORRELATSIOONIMAATRIKS",
                          sidebarLayout(
                            sidebarPanel(
-                            
+                             
                              selectInput(
                                "select1",
-                               "Visualisatsiooni meetod 1:",
-                               choices = list("square" = 1, "circle" = 2),
+                               "Vali maatriksi tüüp:",
+                               choices = list("ruut" = 1, "ring" = 2),
                                selected = 1
                              ),
-                             selectInput("select2", "Visualisatsiooni meetod 2:", choices = list("true" = 1, "false" = 2), 
+                             selectInput("select2", "Vali koefitsientide kuva:", choices = list("koefitsientidega" = 1, "ilma koefitsientideta" = 2), 
                                          selected = 2),
                            ),
-                           mainPanel(h3("Tunnuste korrelatsioonimaatriks:"),
+                           mainPanel(h3("Korrelatsioonimaatriks:"),
                                      
-                                    
+                                     
                                      plotOutput("correlation"),
-                                    br(),
-                                    p("Tunnuse quality on määratletakse järgmised sõltuvused:
+                                     br(),
+                                     p("Tunnuse quality on määratletakse järgmised sõltuvused:
                                        
                                        tunnusega allcohol on keskmine kasvav sõltuvus (r=0.48), mis tähendab, et kõrgemat alkoholisisaldust peetakse üldiselt veini kvaliteetsemaks.
                                      
@@ -103,45 +133,11 @@ ui <- fluidPage(
                                      Tunnuste pH ja fixed.acidity vahel on tugev kahenev sõltuvus (r=-0.68). Tunnuste citric.acid ja volative.acidity vahel on keskmine kahenev sõltuvus (r=-0.55). Tunnuste citric.acid ja ph vahel on keskmine kahenev sõltuvus (r=-0.54). Tunnuste density ja alcohol vahel on keskmine kahenev sõltuvus (r=-0.5).
                                      
                                      Teiste tunnuste vahel on nõrga tugevusega sõltuvused. Mõnedel juhtudel üldse sõltuvus puudub."
-                           ),
-                           br(),
+                                     ),
+                                     br(),
                            )
                          )
-                         ),
-                tabPanel("KIRJELDAV ANALÜÜS",
-                         sidebarLayout(
-                           sidebarPanel(h3("Tunnused:"),
-                           selectInput('selectVar', "", variables, selected = variables[[1]]),
-                           br(),
-                           sliderInput(inputId = "bins",
-                                       label = "Number of bins:",
-                                       min = 1,
-                                       max = 50,
-                                       value = 30),
-                           
-                                                     
-                           
-                           ),
-                           
-                           mainPanel(br(),
-                                     
-                                     fluidRow(
-                                       column(width = 8,
-                                              h3("Histogramm:"),
-                                              plotOutput(outputId = "distPlot"),
-                                       ),
-                                       column(width = 8,
-                                              h3("Boxplot:"),
-                                              plotOutput("boxplot"),
-                                       )
-                                     ),
-                             
-                             
-                             h3("Arvkarakteristikud:"),
-                             br(),    
-                             dataTableOutput('value')
-                             
-                           ) ))
+                )
                 
     )))
    
@@ -162,12 +158,19 @@ output$correlation <- renderPlot({
   else if(input$select1 == 2 && input$select2 == 2) {ggcorrplot(corr, lab = FALSE, method = "circle")}
 })
 
-
-output$value <- renderDataTable( summary(wine), options =
+# Generate an HTML table view of the numerical characteristics ----
+output$table2 <- renderDataTable( summary(wine[2:7]), options =
                                    list(searching = FALSE,ordering=F, 
-                                        dom = 't')
+                                        dom = 't'),
   )
-# 2. Its output type is a plot
+
+# Generate an HTML table view of the numerical characteristics ----
+output$table3 <- renderDataTable( summary(wine[8:13]), options =
+                                    list(searching = FALSE,ordering=F, 
+                                         dom = 't'),
+)
+
+# Generate a histogramm
 output$distPlot <- renderPlot({
 
   req(input$selectVar)
@@ -175,16 +178,18 @@ output$distPlot <- renderPlot({
 
   bins <- seq(min(x), max(x), length.out = input$bins + 1)
   
-  hist(x, breaks = bins, col = "#FFB6C1", border = "grey",
+  hist(x, breaks = bins, col = "#FF336A", border = "black",
        xlab = input$selectVar,
        ylab="Sagedus",
        main = "")
   
 })
+
+# Generate a boxplot 
 output$boxplot <- renderPlot({
   req(input$selectVar)
   x <- wine[[input$selectVar]]
-  boxplot(x, col="#B0C4DE", horizontal=1, xlab=input$selectVar)
+  boxplot(x, col=rgb(0,0,1,0.5), horizontal=1, xlab=input$selectVar)
   points(mean(x), 1, col = "red", pch = 18)
   text(mean(x), 0.95, "mean", col="red", cex=0.5)
 })
@@ -194,4 +199,4 @@ output$boxplot <- renderPlot({
 
 
 
-shinyApp(ui = ui, server = server, options = list(autoWidth = TRUE))
+shinyApp(ui = ui, server = server)
